c3795768c8cdcadca6cd00699b34a7ee7d7ee915
Arailyd <avrcorldr@gmail.com>
2019-10-31 14:55:07 -0400

    commit de video 6

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <div class="my-2">
-      <v-btn class="pink white--text">click me</v-btn>
-    </div>
-    <div class="my-2">
-      <v-btn depressed color="pink">click me</v-btn>
-    </div>
-    <div class="my-2">
-      <v-btn flat color="pink">click me</v-btn>
-    </div>
-    <div class="my-2">
-      <v-btn depressed class="pink white--text">
-        <v-icon left>mdi-email</v-icon>
-        <span>email me</span>
-      </v-btn>
-    </div>
+    <!-- <div class="my-2">
+      <v-btn class="hidden-md-and-down">click me</v-btn>
+    </div> -->
+
+    <!-- <div class="my-2">
+      <v-btn class="hidden-md-and-down">click me</v-btn>
+    </div> -->
 
     <div class="my-2">
-      <v-btn small depressed class="pink white--text">
-        <v-icon left>mdi-email</v-icon>
-        <span>email me</span>
-      </v-btn>
+      <v-btn class="hidden-sm-only">click me</v-btn>
     </div>
 
-    <div class="my-2">
-      <v-btn depressed class="pink white--text">
-        <span>email me</span>
-        <v-icon large right>mdi-email</v-icon>
-        
-      </v-btn>
-    </div>
-
-    <div class="my-2">
-      <v-btn fab depressed small dark color="purple">
-        <v-icon>mdi-cloud-upload</v-icon>
-      </v-btn>
-    </div>
+  
   </div>
 </template>
 
