583dd814ff6e4cce4884fe7377da00e8a93c0750
Arailyd <avrcorldr@gmail.com>
2019-10-30 21:38:47 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+# test-01
+Repositorio de prueba
+=======
+# proyecto1
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+>>>>>>> mi primer commit
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illum laudantium sapiente, consequatur sequi molestiae vero obcaecati nesciunt, deserunt quam expedita aliquam esse recusandae eum a pariatur non, dolore debitis.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae nesciunt ex sint dolorum repellendus corrupti voluptas suscipit expedita. Exercitationem ipsum veritatis, incidunt enim veniam tempore tenetur sunt provident in. Tenetur!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline text-capitalize">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
+    <v-btn class="pink white--text">click me</v-btn>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {};
+</script>
