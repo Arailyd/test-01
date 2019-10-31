e0d049cfbabda4b2bc68e10011f3a443bb3c13e8
Arailyd <avrcorldr@gmail.com>
2019-10-31 10:11:38 -0400

    commit de video 4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red blue--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas eius quam, consectetur eligendi impedit reiciendis natus, voluptates placeat qui aliquid, voluptatibus quidem modi debitis. Rem, eligendi exercitationem. Voluptatibus, dolorem odio?</p>
+    <p class="pink lighten-3 red--text text--darken-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo suscipit, cupiditate officiis enim hic repellendus nobis possimus ut modi sit, commodi autem sapiente explicabo ullam fugiat deserunt magnam. Exercitationem, saepe!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is subheading</p>
+    <p class="caption text-capitalize">This is a caption</p>
   </div>
 </template>
 
