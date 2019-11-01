ec724d12e5348762e4739a4d82b84d9acffc2ca8
Arailyd <avrcorldr@gmail.com>
2019-10-31 20:14:38 -0400

    commit de video 7

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <!-- <div class="my-2">
-      <v-btn class="hidden-md-and-down">click me</v-btn>
-    </div> -->
-
-    <!-- <div class="my-2">
-      <v-btn class="hidden-md-and-down">click me</v-btn>
-    </div> -->
-
-    <div class="my-2">
-      <v-btn class="hidden-sm-only">click me</v-btn>
-    </div>
-
+     
   
   </div>
 </template>
